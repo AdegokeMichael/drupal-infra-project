8a175681499937cd324fabf7ba9d4e52b14de8df
Adegoke <57226067+AdegokeMichael@users.noreply.github.com>
2025-07-24 18:38:43 +0100

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,51 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile"
+  type        = string
+  default     = "default"
+}
+
+variable "project" {
+  description = "Name prefix for project resources"
+  type        = string
+  default     = "drupal-infra"
+}
+
+variable "environment" {
+  description = "Environment tag (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "root_volume_size" {
+  description = "Root volume size for EC2 in GiB"
+  type        = number
+  default     = 30
+}
+
+variable "root_volume_type" {
+  description = "EBS volume type for EC2 root volume"
+  type        = string
+  default     = "gp3"
+}
