5bc42365543194fe2f90649f79dc458af4669ad9
Adegoke <57226067+AdegokeMichael@users.noreply.github.com>
2025-07-26 09:40:21 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,17 @@
+output "rds_endpoint" {
+  value = module.rds.rds_endpoint
+}
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "app_private_ips" {
+  value = aws_instance.app[*].private_ip
+}
+
+output "app_instance_ids" {
+  value = aws_instance.app[*].id
+}
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
