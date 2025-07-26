e9655370389e649423d8b48bf72222e02e85393c
Adegoke <57226067+AdegokeMichael@users.noreply.github.com>
2025-07-26 10:01:53 +0100

    Update variables.tf

--- terraform/modules/security_groups/variables.tf
+++ terraform/modules/security_groups/variables.tf
@@ -12,3 +12,7 @@ variable "bastion_cidr" {
   description = "CIDR block or IP allowed to SSH into EC2 (bastion host)"
   type        = string
 }
+variable "my_ip_cidr" {
+  description = "102.89.22.112/32"
+  type        = string
+}
